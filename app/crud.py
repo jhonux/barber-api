de858935e4f24a47afd94aa00122546e9548e440
jhonux <jhonatah197@gmail.com>
2025-12-14 14:30:57 -0300

    Fix: Corrigindo erro do user_create

--- app/crud.py
+++ app/crud.py
@@ -10,7 +10,7 @@ def get_user_by_email(db: Session, email: str):
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
 
-    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db_user = models.User(email=user.email, name=user.name, hashed_password=hashed_password)
 
     db.add(db_user)
     db.commit()
