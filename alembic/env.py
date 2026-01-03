9063b0a667d15ce34a393c5af699e20559671564
jhonux <jhonatah197@gmail.com>
2026-01-03 15:05:25 -0300

    Correcao env.py final

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,33 +6,24 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# Importa o modelo Base para o autogenerate funcionar
-from app.database import Base
-from app.models import * # Importante para registrar os modelos
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from app.database import Base
+from app.models import * 
+
 config = context.config
 
-# --- CORREÇÃO AQUI ---
-# Define a URL do banco de dados diretamente no objeto de configuração.
-# Isso substitui o valor do alembic.ini
-config.set_main_option("sqlalchemy.url", "postgresql+psycopg2://barber:supersecret@localhost:5432/barberapi")
-# ---------------------
+db_url = os.getenv("DATABASE_URL")
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+if not db_url:
+    db_url = "postgresql+psycopg2://barber:supersecret@localhost:5432/barberapi"
+
+config.set_main_option("sqlalchemy.url", db_url)
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
